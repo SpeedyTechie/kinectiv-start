13da6d26594e2813e747026d14d9fec9a6212b89
Zane Harnish <zane@getkinectiv.com>
2020-07-28 11:26:22 -0400

    Update to latest version of gulp and all dependencies (and rewrite gulpfile to support new versions)

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,46 @@
-var gulp = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var cleancss = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const cleancss = require('gulp-clean-css');
 
-gulp.task('css', function() {
-    return gulp.src('style.css')
-        .pipe(autoprefixer({browsers: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not ie <= 8']}))
-        .pipe(cleancss({level: 1, rebase: false, inline: false}))
+
+function css() {
+    return src('style.css')
+        .pipe(autoprefixer())
+        .pipe(cleancss({rebase: false, inline: false, compatibility: 'ie9'}))
         .pipe(rename('style.min.css'))
-        .pipe(gulp.dest(''));
-});
+        .pipe(dest('.'));
+}
 
-gulp.task('vendor-css', function() {
-    return gulp.src('css/src/*.css')
+function vendorCss() {
+    return src('css/src/*.css')
         .pipe(concat('vendor.css'))
-        .pipe(autoprefixer({browsers: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'not ie <= 8']}))
-        .pipe(gulp.dest('css'))
+        .pipe(autoprefixer())
+        .pipe(dest('css'))
         .pipe(rename('vendor.min.css'))
-        .pipe(cleancss({level: 1, rebase: false, inline: false}))
-        .pipe(gulp.dest('css'));
-});
+        .pipe(cleancss({rebase: false, inline: false, compatibility: 'ie9'}))
+        .pipe(dest('css'));
+}
 
-gulp.task('js', function() {
-    return gulp.src(['js/src/@(!(functions)*|functions+(?)).js', 'js/src/functions.js'])
+function js() {
+    return src(['js/src/@(!(functions)*|functions+(?)).js', 'js/src/functions.js'])
         .pipe(concat('script.js'))
-        .pipe(gulp.dest('js'))
+        .pipe(dest('js'))
         .pipe(rename('script.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js'));
-});
+        .pipe(dest('js'));
+}
 
-gulp.task('watch', function() {
-    gulp.watch('css/src/*.css', ['vendor-css']);
-    gulp.watch('style.css', ['css']);
-    gulp.watch('js/src/*.js', ['js']);
-});
+function monitor(cb) {
+    watch('style.css', css);
+    watch('css/src/*.css', vendorCss);
+    watch('js/src/*.js', js);
+    
+    cb();
+}
 
-gulp.task('default', ['css', 'vendor-css', 'js', 'watch']);
+
+exports.default = series(parallel(css, vendorCss, js), monitor);
