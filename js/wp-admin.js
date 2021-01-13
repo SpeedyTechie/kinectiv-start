d6e0682633d30c7c82bc57b22fe3009778255ba5
Zane Harnish <zane@getkinectiv.com>
2021-01-13 15:03:45 -0500

    Update admin js to new ACF js API

--- js/wp-admin.js
+++ js/wp-admin.js
@@ -1,7 +1,7 @@
 /* Disable the wpview TinyMCE plugin for ACF WYSIWYG fields (unless the enabled in settings) */
 
-acf.add_filter('wysiwyg_tinymce_settings', function(mceInit, id, $field){
-    if ($field.hasClass('ks-disable-autoembed')) {
+acf.addFilter('wysiwyg_tinymce_settings', function(mceInit, id, field){
+    if (field.$el.hasClass('ks-disable-autoembed')) {
         var plugins = mceInit['plugins'].split(',');
         var wpviewIndex = plugins.indexOf('wpview');
         
